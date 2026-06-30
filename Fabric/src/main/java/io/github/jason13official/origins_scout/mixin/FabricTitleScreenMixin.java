21c1b21732d36331b786748750e2395e02bf1531
jason13official <jason13official@gmail.com>
2026-06-29 21:28:20 -0400

    tweak

---
+++ Fabric/src/main/java/io/github/jason13official/origins_scout/mixin/FabricTitleScreenMixin.java
@@ -0,0 +1,24 @@
+package io.github.jason13official.origins_scout.mixin;
+
+import io.github.jason13official.origins_scout.Constants;
+import io.github.jason13official.origins_scout.platform.Services;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screens.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class FabricTitleScreenMixin {
+
+  @Inject(at = @At("HEAD"), method = "init()V")
+  private void init(CallbackInfo info) {
+
+    if (Services.PLATFORM.isDevelopmentEnvironment()) {
+      Constants.LOG.info("This line is printed by an example mod mixin from Fabric!");
+      Constants.LOG.info("MC Version: {}", Minecraft.getInstance().getVersionType());
+      Constants.LOG.info("Classloader: {}", this.getClass().getClassLoader());
+    }
+  }
+}
--- Forge/src/main/java/io/github/jason13official/origins_scout/OriginsScoutForge.java
+++ Forge/src/main/java/io/github/jason13official/origins_scout/OriginsScoutForge.java
@@ -1,33 +1,22 @@
 package io.github.jason13official.origins_scout;
 
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.entity.player.ItemTooltipEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(Constants.MOD_ID)
 public class OriginsScoutForge {
-    
-    public OriginsScoutForge() {
-    
-        // This method is invoked by the Forge mod loader when it is ready
-        // to load your mod. You can access Forge and Common code in this
-        // project.
-    
-        // Use Forge to bootstrap the Common mod.
-        Constants.LOG.info("Hello Forge world!");
-        OriginsScout.init();
-    
-        // Some code like events require special initialization from the
-        // loader specific code.
-        MinecraftForge.EVENT_BUS.addListener(this::onItemTooltip);
-        
-    }
-    
-    // This method exists as a wrapper for the code in the Common project.
-    // It takes Forge's event object and passes the parameters along to
-    // the Common listener.
-    private void onItemTooltip(ItemTooltipEvent event) {
-        
-        OriginsScout.onItemTooltip(event.getItemStack(), event.getFlags(), event.getToolTip());
-    }
+
+  private static IEventBus EVENT_BUS;
+
+  public OriginsScoutForge(FMLJavaModLoadingContext context) {
+
+    EVENT_BUS = context.getModEventBus();
+    OriginsScout.preInit();
+  }
+
+  @Deprecated
+  public OriginsScoutForge() {
+    this(FMLJavaModLoadingContext.get());
+  }
 }
